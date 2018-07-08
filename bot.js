accef3ed722fd493ca7925db74fb13c7af24f982
yoterz <40376255+yoterz@users.noreply.github.com>
2018-07-08 23:27:39 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -46,5 +46,64 @@ function status(){
     bot.user.setGame('NEXT '+chkboss().t+' '+chkboss().boss)
     alertz()
 }
+function alertz() {
+    day = thtime().day
+    h = thtime().h
+    m = thtime().m
+    
+  if (day == 2 || day == 3 || day == 6) {
+    if (h == 23 && m == 45) {
+      var channel = bot.channels.find("name", channelName)
+      if (channel) {
+        channel.sendMessage('@everyone '+chkboss().boss+' อีก 15 นาที')
+      } else {
+        var channel = bot.channels.find("name", 'general')
+        channel.sendMessage('```-Create Channel-```')
+      }
+    }
+  }
+  if (day == 0 || day == 1 || day == 4 || day == 5) {
+    if (h == 9 && m == 45) {
+      var channel = bot.channels.find("name", channelName)
+      if (channel) {
+        channel.sendMessage('@everyone '+chkboss().boss+' อีก 15 นาที')
+      } else {
+        var channel = bot.channels.find("name", 'general')
+        channel.sendMessage('```-Create Channel-```')
+      }
+    }
+  }
+  if (h == 13 && m == 45) {
+    var channel = bot.channels.find("name", channelName)
+    if (channel) {
+      channel.sendMessage('@everyone '+chkboss().boss+' อีก 15 นาที')
+    } else {
+      var channel = bot.channels.find("name", 'general')
+      channel.sendMessage('```-Create Channel-```')
+    }
+  }
+  if (h == 17 && m == 45) {
+    var channel = bot.channels.find("name", channelName)
+    if (channel) {
+      channel.sendMessage('@everyone '+chkboss().boss+' อีก 15 นาที')
+    } else {
+      var channel = bot.channels.find("name", 'general')
+      channel.sendMessage('```-Create Channel-```')
+    }
+  }
+    
+    
+   if (h == 23 && m == 24) {
+    var channel = bot.channels.find("name", channelName)
+    if (channel) {
+      channel.sendMessage('@everyone '+chkboss().boss+' อีก 15 นาที')
+    } else {
+      var channel = bot.channels.find("name", 'general')
+      channel.sendMessage('```-Create Channel-```')
+    }
+  }
+    
+    
+}
 
 bot.login(process.env.BOT_TOKEN)
