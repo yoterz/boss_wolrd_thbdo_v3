a7a0b9ba802ae9fd25741812468c0b0b516f6470
yoterz <40376255+yoterz@users.noreply.github.com>
2018-07-09 11:51:20 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,6 +2,7 @@ const Discord = require("discord.js")
 const thtime = require("./thtime")
 const chkboss = require("./chkboss")
 const botconfig = require("./botconfig")
+var sleep = require('sleep');
 var bot = new Discord.Client()
 var channelName = botconfig.channelName
 bot.on("ready",function(){
@@ -30,11 +31,12 @@ bot.on("message", (msg) => {
     
     if (msg.content === "```-Create Channel-```") {
          msg.guild.createChannel(channelName, 'text')   //สร้างห้องง
-         //var channel = bot.channels.find("name", channelName)
-         if (bot.channels.find("name", channelName)) {
+         sleep.sleep(3)
+         var channel = bot.channels.find("name", channelName)
+         if (channel) {
              msg.channel.send('```' + 'สร้างห้อง ' + channelName + ' เรียบร้อย' + '```')
          }else{
-             msg.channel.send('```' + 'สร้างห้อง ' + channelName + ' ไม่ได้ กรุณาให้ยศแอดมินกับบอท' + '```')
+             msg.channel.send('```' + 'สร้างห้อง ' + channelName + ' ไม่ได้ กรุณาให้ยศAdminกับบอท' + '```')
          }
     }
 
