fabd512e9f7a8f53e79360b7d6f6c2bc3bf06ad1
yoterz <40376255+yoterz@users.noreply.github.com>
2018-07-26 11:11:46 +0700

    Update databoss.js

--- databoss.js
+++ databoss.js
@@ -3,16 +3,16 @@ const pic = require("./pic")
 exports.day0 = 
     {
         b00 : ['คจาคาร์',pic.kzarka],
-        b10 : ['คจาคาร์',pic.kzarka],
+        b10 : ['คจาคาร์,นูเวอร์',pic.noukar],
         b14 : ['คูทุม',pic.kutum],
-        b18 : ['คจาคาร์',pic.kzarka]
+        b18 : ['คจาคาร์,นูเวอร์',pic.noukar]
     }
 
 exports.day1 = 
     {
         b00 : ['ไม่มี',pic.noboss],
         b10 : ['คูทุม',pic.kutum],
-        b14 : ['คจาคาร์',pic.kzarka],
+        b14 : ['คจาคาร์,นูเวอร์',pic.noukar],
         b18 : ['คูทุม',pic.kutum]
     }
 
@@ -21,13 +21,13 @@ exports.day2 =
        b00 : ['ไม่มี',pic.noboss],
        b10 : ['ไม่มี',pic.noboss],
        b14 : ['คูทุม',pic.kutum],
-       b18 : ['คจาคาร์',pic.kzarka]
+       b18 : ['คจาคาร์,นูเวอร์',pic.noukar]
     }
 
 exports.day3 = 
     {
         b00 : ['คจาคาร์',pic.kzarka],
-        b10 : ['ไม่มี',pic.noboss],
+        b10 : ['นูเวอร์',pic.nouver],
         b14 : ['คจาคาร์',pic.kzarka],
         b18 : ['คูทุม',pic.kutum]
     }
@@ -43,15 +43,15 @@ exports.day4 =
 exports.day5 = 
     {
         b00 : ['ไม่มี',pic.noboss],
-        b10 : ['คูทุม',pic.kutum],
+        b10 : ['คูทุม,นูเวอร์',pic.noutum],
         b14 : ['คจาคาร์',pic.kzarka],
-        b18 : ['คูทุม',pic.kutum]
+        b18 : ['คูทุม,นูเวอร์',pic.noutum]
     }
  
 exports.day6 = 
     {
         b00 : ['ไม่มี',pic.noboss],
         b10 : ['ไม่มี',pic.noboss],
-        b14 : ['คูทุม',pic.kutum],
-        b18 : ['คจาคาร์',pic.kzarka]
+        b14 : ['คูทุม,นูเวอร์',pic.noutum],
+        b18 : ['คจาคาร์,นูเวอร์',pic.noukar]
     }
