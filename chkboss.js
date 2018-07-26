fd69006cf22f913e522cd829281c2572dd8779e6
yoterz <40376255+yoterz@users.noreply.github.com>
2018-07-26 12:35:05 +0700

    Update chkboss.js

--- chkboss.js
+++ chkboss.js
@@ -7,7 +7,17 @@ module.exports = function() {
     var h = timestart().h
     switch (day) {
     /*อาทิตย์*/   case 0 :
-                  if (h >= 0 && h < 10){
+                  if (h == 0 && m < 15){                          //< 00.15 น
+                    return{
+                             boss : databoss.day0.b00[0],         
+                             t : '00:15',
+                             imgboss : databoss.day0.b00[1] }
+                  }else if (h == 0 && m >= 15){                   //> 00.15 น - 01.00 น
+                    return{
+                             boss : databoss.day0.b00[0],         
+                             t : '00:15',
+                             imgboss : databoss.day0.b00[1] }
+                  }else if (h >= 1 && h < 10){
                     return{
                              boss : databoss.day0.b10[0],
                              t : '10:00',
@@ -25,12 +35,22 @@ module.exports = function() {
                   }else if (h >= 18 && h < 24) {
                     return{
                              boss : databoss.day1.b00[0],         //ตัวของเช้าวัน  จันทร์
-                             t : '00:00',
+                             t : '00:15',
                              imgboss : databoss.day1.b00[1] }
                   }
                   break   
     /*จันทร์*/    case 1:
-                  if (h >= 0 && h < 10){
+                if (h == 0 && m < 15){                          //< 00.15 น
+                    return{
+                             boss : databoss.day1.b00[0],         
+                             t : '00:15',
+                             imgboss : databoss.day1.b00[1] }
+                  }else if (h == 0 && m >= 15){                   //> 00.15 น - 01.00 น
+                    return{
+                             boss : databoss.day1.b00[0],         
+                             t : '00:15',
+                             imgboss : databoss.day1.b00[1] }
+                  }else if (h >= 1 && h < 10){
                     return{
                              boss : databoss.day1.b10[0],
                              t : '10:00',
@@ -53,7 +73,17 @@ module.exports = function() {
                   }
                   break
     /*อังคาร*/   case 2:
-                  if (h >= 0 && h < 10){
+                  if (h == 0 && m < 15){                          //< 00.15 น
+                    return{
+                             boss : databoss.day2.b00[0],         
+                             t : '00:15',
+                             imgboss : databoss.day2.b00[1] }
+                  }else if (h == 0 && m >= 15){                   //> 00.15 น - 01.00 น
+                    return{
+                             boss : databoss.day2.b00[0],         
+                             t : '00:15',
+                             imgboss : databoss.day2.b00[1] }   
+                  }else if (h >= 1 && h < 10){                      //1.00 - 10.00
                     return{
                              boss : databoss.day2.b10[0],
                              t : '10:00',
@@ -76,7 +106,17 @@ module.exports = function() {
                   }
                   break
     /*พุธ*/      case 3 :
-                  if (h >= 0 && h < 10){
+                  if (h == 0 && m < 15){                          //< 00.15 น
+                    return{
+                             boss : databoss.day3.b00[0],         
+                             t : '00:15',
+                             imgboss : databoss.day3.b00[1] }
+                  }else if (h == 0 && m >= 15){                   //> 00.15 น - 01.00 น
+                    return{
+                             boss : databoss.day3.b00[0],         
+                             t : '00:15',
+                             imgboss : databoss.day3.b00[1] }
+                  }else if (h >= 1 && h < 10){
                     return{
                              boss : databoss.day3.b10[0],
                              t : '10:00',
@@ -99,7 +139,17 @@ module.exports = function() {
                   }
                   break
     /*พฤ*/      case 4 :
-                  if (h >= 0 && h < 10){
+                  if (h == 0 && m < 15){                          //< 00.15 น
+                    return{
+                             boss : databoss.day4.b00[0],         
+                             t : '00:15',
+                             imgboss : databoss.day4.b00[1] }
+                  }else if (h == 0 && m >= 15){                   //> 00.15 น - 01.00 น
+                    return{
+                             boss : databoss.day4.b00[0],         
+                             t : '00:15',
+                             imgboss : databoss.day4.b00[1] }
+                  }else if (h >= 1 && h < 10){
                     return{
                              boss : databoss.day4.b10[0],
                              t : '10:00',
@@ -122,7 +172,17 @@ module.exports = function() {
                   }
                   break
     /*ศุกร์*/     case 5 :
-                  if (h >= 0 && h < 10){
+                  if (h == 0 && m < 15){                          //< 00.15 น
+                    return{
+                             boss : databoss.day5.b00[0],         
+                             t : '00:15',
+                             imgboss : databoss.day5.b00[1] }
+                  }else if (h == 0 && m >= 15){                   //> 00.15 น - 01.00 น
+                    return{
+                             boss : databoss.day5.b00[0],         
+                             t : '00:15',
+                             imgboss : databoss.day5.b00[1] }
+                  }else if (h >= 1 && h < 10){
                     return{
                              boss : databoss.day5.b10[0],
                              t : '10:00',
@@ -145,7 +205,17 @@ module.exports = function() {
                   }
                   break
     /*เสาร์*/     case 6 :
-                  if (h >= 0 && h < 10){
+                  if (h == 0 && m < 15){                          //< 00.15 น
+                    return{
+                             boss : databoss.day6.b00[0],         
+                             t : '00:15',
+                             imgboss : databoss.day6.b00[1] }
+                  }else if (h == 0 && m >= 15){                   //> 00.15 น - 01.00 น
+                    return{
+                             boss : databoss.day6.b00[0],         
+                             t : '00:15',
+                             imgboss : databoss.day6.b00[1] }
+                  }else if (h >= 1 && h < 10){
                     return{
                              boss : databoss.day6.b10[0],
                              t : '10:00',
