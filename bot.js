ac767631d989c82a558722599bfef9b424dd4d91
yoterz <40376255+yoterz@users.noreply.github.com>
2018-07-27 12:59:57 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -25,74 +25,47 @@ bot.on("message", (msg) => {
         .setImage(pic.tableboss)     //รูปใหญ่
         .setThumbnail(chkboss().imgboss)   //รูปเล็กขวาบนผ
         .setTimestamp()  //เวลาด้านล่างสุดผ
-         msg.channel.sendMessage({embed})
+         msg.channel.send({embed})
     }
-    
-    
-    if (msg.content === "```-Create Channel- ถ้าสร้างไม่ได้กรุณาให้ยศAdminกับบอท```") {
-         msg.guild.createChannel(channelName, 'text')   //สร้างห้องง
-    }
-
+ 
 })
+
 
 function status(){     
     bot.user.setGame('NEXT '+chkboss().t+' '+chkboss().boss)
     chkchannel()
 }
 
-function chkchannel(){
-      var channel = bot.channels.find("name", channelName)
-      if (!channel) {
-        var channel = bot.channels.find("name", 'general')
-        bot.channels.get(channel.id).send('```-Create Channel- ถ้าสร้างไม่ได้กรุณาให้ยศAdminกับบอท```')
-        //channel.sendMessage('```-Create Channel-```')
-      } 
-}
 
 function alertz() {
     day = thtime().day
     h = thtime().h
     m = thtime().m
   
-    var channel = bot.channels.find("name", channelName)
-  if (!channel) {
-        var channel = bot.channels.find("name", 'general')
-        channel.sendMessage('```-Create Channel- ถ้าสร้างไม่ได้กรุณาให้ยศAdminกับบอท```')
-  } 
+  var channel = bot.channels.find("name", channelName)
         
   if (day == 0 || day == 3 || day == 4) {
     if (h == 0 && m == 0) {
-      channel.sendMessage('@everyone '+chkboss().boss+' อีก 15 นาที')
+      channel.send('@everyone '+chkboss().boss+' อีก 15 นาที')
     }
   }
     
   if (day == 0 || day == 1 || day == 3 || day == 4 || day == 5) {
     if (h == 9 && m == 45) {
-      channel.sendMessage('@everyone '+chkboss().boss+' อีก 15 นาที')
+      channel.send('@everyone '+chkboss().boss+' อีก 15 นาที')
      }
   }
     
   if (h == 13 && m == 45) {
-      channel.sendMessage('@everyone '+chkboss().boss+' อีก 15 นาที')
+      channel.send('@everyone '+chkboss().boss+' อีก 15 นาที')
   }
     
   if (h == 17 && m == 45) {
-      channel.sendMessage('@everyone '+chkboss().boss+' อีก 15 นาที')
+      channel.send('@everyone '+chkboss().boss+' อีก 15 นาที')
   }
- 
- /* if (h == 0 && m == 56) {  //เทสส่ง msg
-      channel.sendMessage('@everyone test message to channel : '+channelName+' / Next boss : '+chkboss().boss)
-  } //จบ เทส*/
-    
+   
 }
 
-function sleep(milliseconds) {
-    var start = new Date().getTime();
-    for (var i = 0; i < 1e7; i++) {
-      if ((new Date().getTime() - start) > milliseconds){
-        break;
-      }
-    }
-  }
+
 
 bot.login(process.env.BOT_TOKEN)
