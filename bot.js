0e8f9806cf81cc89e7df81c2db78f5c31e6f7a7b
yoterz <40376255+yoterz@users.noreply.github.com>
2018-07-27 18:02:45 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -23,7 +23,6 @@ bot.on("message", (msg) => {
         .setAuthor("Boss Timer ThBDO", "https://www.picz.in.th/images/2018/06/22/48XhJt.png")  //icon หัวขอ
         .setColor(0x112263)   //ใส่สี
         .setDescription("```md\n"+"วัน "+thtime().days+" เวลา <"+chkboss().t+"> * "+chkboss().boss+" *```")   //รายละเอียด
-      // .addField("บอสตัวต่อไป","```md\n"+"วัน "+thtime().days+" เวลา <"+chkboss().t+"> * "+chkboss().boss+" *```")
         .setFooter("Boss Timer ThBDO "+botconfig.version+" by ฟูโอ้", "https://upload.wikimedia.org/wikipedia/commons/thumb/7/7e/Twemoji_1f437.svg/2000px-Twemoji_1f437.svg.png") //รูป ข้อความล่างสุด
         .setImage(pic.tableboss)     //รูปใหญ่
         .setThumbnail(chkboss().imgboss)   //รูปเล็กขวาบนผ
@@ -41,37 +40,27 @@ function alertz() {
     day = thtime().day
     h = thtime().h
     m = thtime().m
-  
-  //var channel = bot.channels.find("name", channelName)
-        
+         
   if (day == 0 || day == 3 || day == 4) {
     if (h == 0 && m == 0) {
         sendallchannel()
-        //channel.send('@everyone '+chkboss().boss+' อีก 15 นาที')
     }
   }
     
   if (day == 0 || day == 1 || day == 3 || day == 4 || day == 5) {
     if (h == 9 && m == 45) {
         sendallchannel()
-      //channel.send('@everyone '+chkboss().boss+' อีก 15 นาที')
      }
   }
     
   if (h == 13 && m == 45) {
       sendallchannel()
-      //channel.send('@everyone '+chkboss().boss+' อีก 15 นาที')
   }
     
   if (h == 17 && m == 45) {
       sendallchannel()
-      //channel.send('@everyone '+chkboss().boss+' อีก 15 นาที')
   }
-    //test msg
-    if (h == 17 && m == 35) {
-      sendallchannel()
-     }
-   
+       
 }
 
 function sendallchannel(){    //ส่งทุกห้องที่มีบอทอยู่
