9555b7c424b1c003ea4d2e81a0d0fa65c816f829
yoterz <40376255+yoterz@users.noreply.github.com>
2018-08-16 08:14:11 +0700

    Update databoss.js

--- databoss.js
+++ databoss.js
@@ -2,56 +2,70 @@ const pic = require("./pic")
 
 exports.day0 = 
     {
-        b00 : ['คจาคาร์',pic.kzarka],
-        b10 : ['คจาคาร์,นูเวอร์',pic.noukar],
-        b14 : ['คูทุม',pic.kutum],
-        b18 : ['คจาคาร์,นูเวอร์',pic.noukar]
+        b00 : ['คจาคาร์,คารานด้า',pic.karankar],
+        b06 : ['คูทุม',pic.karanda],
+        b10 : ['คจาคาร์,คารานด้า',pic.karankar],
+        b14 : ['คูทุม,นูเวอร์',pic.noutum],
+        b19 : ['คารานด้า',pic.karanda],
+        b23 : ['นูเวอร์',pic.nouver]
     }
 
 exports.day1 = 
     {
-        b00 : ['ไม่มี',pic.noboss],
-        b10 : ['คูทุม',pic.kutum],
-        b14 : ['คจาคาร์,นูเวอร์',pic.noukar],
-        b18 : ['คูทุม',pic.kutum]
+        b00 : ['คจาคาร์',pic.kzarka],
+        b06 : ['คารานด้า',pic.karanda],
+        b10 : ['ไม่มี',pic.noboss],
+        b14 : ['นูเวอร์',pic.nouver],
+        b19 : ['คจาคาร์',pic.kzarka],
+        b23 : ['คูทุม',pic.kutum] 
     }
 
 exports.day2 = 
     {
-       b00 : ['ไม่มี',pic.noboss],
-       b10 : ['ไม่มี',pic.noboss],
-       b14 : ['คูทุม',pic.kutum],
-       b18 : ['คจาคาร์,นูเวอร์',pic.noukar]
+        b00 : ['นูเวอร์',pic.nouver],
+        b06 : ['คูทุม',pic.kutum],
+        b10 : ['คจาคาร์,คารานด้า',pic.karankar],
+        b14 : ['คูทุม',pic.kutum],
+        b19 : ['นูเวอร์',pic.nouver],
+        b23 : ['นูเวอร์',pic.nouver] 
     }
 
 exports.day3 = 
     {
         b00 : ['คจาคาร์',pic.kzarka],
-        b10 : ['นูเวอร์',pic.nouver],
-        b14 : ['คจาคาร์',pic.kzarka],
-        b18 : ['คูทุม',pic.kutum]
+        b06 : ['นูเวอร์',pic.nouver],
+        b10 : ['คจาคาร์',pic.kzarka],
+        b14 : ['คารานด้า',pic.karanda],
+        b19 : ['คูทุม',pic.kutum],
+        b23 : ['คจาคาร์',pic.kzarka] 
     }
 
 exports.day4 = 
     {
         b00 : ['คูทุม',pic.kutum],
+        b06 : ['ไม่มี',pic.noboss],
         b10 : ['คจาคาร์',pic.kzarka],
-        b14 : ['คูทุม,นูเวอร์',pic.noutum],
-        b18 : ['คจาคาร์',pic.kzarka]
+        b14 : ['คูทุม',pic.kutum],
+        b19 : ['คจาคาร์,คารานด้า',pic.karankar],
+        b23 : ['นูเวอร์',pic.nouver] 
     }
 
 exports.day5 = 
     {
         b00 : ['ไม่มี',pic.noboss],
-        b10 : ['คูทุม,นูเวอร์',pic.noutum],
+        b06 : ['ไม่มี',pic.noboss],
+        b10 : ['คูทุม',pic.kutum],
         b14 : ['คจาคาร์',pic.kzarka],
-        b18 : ['คูทุม,นูเวอร์',pic.noutum]
+        b19 : ['นูเวอร์',pic.nouver],
+        b23 : ['คารานด้า',pic.karanda] 
     }
  
 exports.day6 = 
     {
-        b00 : ['ไม่มี',pic.noboss],
-        b10 : ['ไม่มี',pic.noboss],
-        b14 : ['คูทุม,นูเวอร์',pic.noutum],
-        b18 : ['คจาคาร์,นูเวอร์',pic.noukar]
+        b00 : ['คูทุม',pic.kutum],
+        b06 : ['นูเวอร์',pic.nouver],
+        b10 : ['คูทุม',pic.kutum],
+        b14 : ['คาจาคาร์,นูเวอร์',pic.noukar],
+        b19 : ['คารานด้า',pic.karanda],
+        b23 : ['ไม่มี',pic.noboss] 
     }
