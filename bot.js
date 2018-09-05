193152a35751edd2caa64d2a7c2e40d8d5501f1c
yoterz <40376255+yoterz@users.noreply.github.com>
2018-09-05 09:09:37 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -66,7 +66,7 @@ function alertz() {
     }
   }
     
- if (day == 0 || day == 1 || day == 2 || day == 3 || day == 6) {
+ if (day == 0 || day == 1 || day == 2 || day == 4 || day == 6) {
     if (h == 5 && m == 45) {
         sendAllServer()
     }
