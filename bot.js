1b0291a309c0996465cac295e12cd1ecf2d51af3
yoterz <40376255+yoterz@users.noreply.github.com>
2018-11-19 10:09:24 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -17,7 +17,7 @@ setInterval(()=>status(), 10000);
 //setInterval(()=>alertz(), 60000);
 
 bot.on("message", (msg) => {
-    if (msg.content === "บอส"){
+    if (msg.content === "!บอส"){
         h = thtime().h
         if (chkboss().t === "00:30" && h == 23){       //เช็คเวลาบอสตัวต่อไป ถ้าเป็นเวลา 00.30 และ 23 น ให้ใชวันพรุ่งนี้
                 var dayz = thtime().dayz
